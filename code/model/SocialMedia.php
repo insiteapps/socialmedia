7d3dfed4761c3a0a675fa43cfd3ed90d09cd7429
Itayi Patrick Chitovoro <patrick@chitosystems.com>
2017-02-16 08:39:37 +0200

    code

--- code/model/SocialMedia.php
+++ code/model/SocialMedia.php
@@ -17,7 +17,7 @@ class SocialMedia extends DataObject
     private static $has_one = array(
         "SiteConfig" => "SiteConfig",
         "Page" => "Page",
-        //"Icon" => "Image",
+        "Icon" => "FiitedImage",
     );
 
     public function getCMSFields()
